80dbbd5fa19bdc9e070ec469ecc377ff8e16ec9f
Pavel Vergeev <vergeev@icloud.com>
2019-10-12 19:35:37 +0300

    Add bs4 styles

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,22 +1,10 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(
-        max_length=30,
-        widget=forms.TextInput(
-            attrs={
-                'style': 'border-color: blue',
-                'placeholder': 'Write your name here'
-            }
-        )
-    )
-    email = forms.EmailField(
-        max_length=254,
-        widget=forms.EmailInput(attrs={'style': 'border-color: green;'})
-    )
+    name = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=254)
     message = forms.CharField(
         max_length=2000,
-        widget=forms.Textarea(attrs={'style': 'border-color: orange;'}),
         help_text='Write your message here!'
     )
 
